3bc0d422470e54cd6155d41a5ad5d04e13b689aa
RH Negative <sick_libra@hotmail.com>
2024-05-02 14:06:29 -0400

    Working in GUI model

---
+++ Serialize_GUI.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+
+
+public class Serialize_GUI {
+    public static void main(String[] args) {
+        JFrame frame=new JFrame("Game Catalog");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        JButton exit =new JButton(Quit);
+        JButton save =new JButton ("Save");
+        frame.getContentPane().add(exit);
+        
+        
+    }
+    
+}
