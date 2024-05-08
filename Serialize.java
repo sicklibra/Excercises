05a1831263b5705dfb85337872f934e5b0430ea2
RH Negative <sick_libra@hotmail.com>
2024-05-08 09:37:16 -0400

    Successfully implimented deserialize method added comments

--- Serialize.java
+++ Serialize.java
@@ -1,3 +1,4 @@
+import javax.swing.border.TitledBorder;
 import java.lang.reflect.Array;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -6,11 +7,14 @@ import java.nio.file.Path;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 /*This is the text based version of serialize for proof of concept*/
 
-//This method will write the array list to csv
+
 public class Serialize {
+    /*This Method takes all of the games from the to string method of each game and turns it into 1 large string which
+    * includes the line breaks sent over from the object method. Each game is separated by line in the csv.*/
     public static void save(ArrayList<Game> games) {
         Path filePath = Paths.get("gamelog.csv");
         int counter = 0;
@@ -18,7 +22,7 @@ public class Serialize {
         String bigString="";
         try {//This trys to run the loop
             for (Game x : games) {
-                bigString=bigString+x.toString();
+                bigString=bigString+x.csvStr();
                     counter++;
             }
         }
@@ -35,10 +39,36 @@ public class Serialize {
             System.out.println("You have successfully written " + counter + " games into the system.");
         }
     }
-
-    public static void deserialize(ArrayList<Game> games) {
-        Path filepath =Paths.get("gamelog.csv");
+/*This function reads everything from the CSV and re-maps it to objects and repopulates the games array list
+* from the last point that was saved. it does this by reading each line into a list and then separating the
+* values by commas and recreates each game and adds it to the list.*/
+    public static ArrayList<Game> deserialize(ArrayList<Game> games) {
+        String title;
+        String system;
+        int numplayers;
+        String format;
+        try{
+            List<String> lines = Files.readAllLines(Paths.get("gamelog.csv"), StandardCharsets.UTF_8);
+            for (String line : lines) {
+                String[] split = line.split(",");
+                title = split[0];
+                system = split[1];
+                numplayers = Integer.parseInt(split[2]);
+                format = split[3];
+                games.add(new Game(title, system, numplayers, format));
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return games;
     }
+    /*This method was separated from main in an effort to simplify main and streamline the process. It takes user input
+    * and creates games from it. separating it also allowed me to easily stop the function if the title input was
+    * unacceptable. */
     public static ArrayList<Game> entry(ArrayList<Game> games){
         Scanner scnr =new Scanner(System.in);
         String title;
@@ -47,6 +77,7 @@ public class Serialize {
         String format;
         System.out.println("What is the title of the game?");
         title=scnr.nextLine();
+        //This clause kicks back to the main function if no title is entered.
         if(title.isEmpty()){
             System.out.println("The game must have a title! \n Try again!");
             return games;
@@ -75,8 +106,9 @@ public class Serialize {
         Scanner scnr = new Scanner(System.in);
         String cont = "y";
         ArrayList<Game> games = new ArrayList<Game>();
-        //games=deserialize(games);
-
+        //Pulls from the CSV to pick up where We last left off.
+        deserialize(games);
+        //This allows for the repetitive entry of games to be saved as long as the user continues to enter "y".
         while (cont.equalsIgnoreCase("y")) {
             System.out.println("Would you like to enter a new game to the Database? y/n");
             try {
@@ -97,7 +129,6 @@ public class Serialize {
         cont=scnr.next();
         if (cont.equalsIgnoreCase("y")){
             save(games);
-
         }
     }
 }
